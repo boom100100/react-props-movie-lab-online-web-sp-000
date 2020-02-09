9ffe001174dbe8b3347c24d4d3641567892037bd
Bernadette Davis <bsd231@nyu.edu>
2020-02-09 18:06:53 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,7 +5,7 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    return movieData.map(x => {return <MovieCard title=x.title IMDBRating=x.IMDBRating genres=x.genres poster=x.poster/>});
+    return movieData.map(x => {return <MovieCard title={x.title} IMDBRating={x.IMDBRating} genres={x.genres} poster={x.poster} />});
   }
 
   render() {
